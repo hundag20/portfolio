6a3dd4598b2231dfe3fa5e5a571e9308a0ec8f48
Hunda guluma Sobokssa <hundag@coopbank.local>
2023-02-28 15:54:46 +0300

    added statement on about me

--- src/profile.js
+++ src/profile.js
@@ -20,7 +20,7 @@ const background = {
 const section2title = 'About Me';
 const about = {
   paragraph:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris nunc congue nisi vitae suscipit tellus mauris a diam. ',
+    'I\'m a full-stack dev primarily working on Javascript/Typescript. I am fairly comfortable with the modern web development ecosystem and I can see myself working in any kind of web based project.',
 };
 // Edit your skill and the percentage you know about it
 // To Add a skill, copy any one below and paste it after the last comma
